4cf91f13b77e485e622ef1e9d6160ec0693057bb
Aneesh Naik <aneesh92@hotmail.co.uk>
2019-03-21 12:30:08 +0000

    Restructing kwargs in lnlike function

--- fit/__init__.py
+++ fit/__init__.py
@@ -9,11 +9,10 @@ import sys
 import time
 import emcee
 import numpy as np
-import spam.data
 from scipy.constants import G
 from scipy.constants import parsec as pc
-from v_model import v_model
-from priors import lnprior
+from .v_model import v_model
+from .priors import lnprior
 
 # physical constants
 delta = 93.6
@@ -35,27 +34,31 @@ class GalaxyFit:
         Number of MCMC walkers. Default is 30.
     ntemps : int
         Number of MCMC temperatures. Default is 4.
-    fR : bool
+    **model : str, {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H0'-'H19', 'I0'-'I19'}
+        Which preset model to use for fit. All settings below are prechosen.
+        See Table 1 in Naik et al. (2019) for details about all models. If
+        choosing a model, no other kwargs should be provided.
+    **fR : bool
         Whether an f(R) gravity 5th force is included. Default is False.
-    halo_type : str, {'NFW', 'DC14'}
+    **halo_type : str, {'NFW', 'DC14'}
         Whether DM haloes are modelled as NFW or DC14. Default is 'NFW'.
-    upsilon : str, {'single', 'double', 'fixed'}
+    **upsilon : str, {'single', 'double', 'fixed'}
         Whether to have a single free parameter for the mass-to-light ratio,
         two free parameters, or fixed empirical values. Default is 'single'.
-    baryon_bound : bool
+    **baryon_bound : bool
         Whether baryon fraction in galaxies is capped at the cosmic baryon
         fraction. Default is True.
-    scaling_priors : bool
+    **scaling_priors : bool
         Whether stellar mass / halo mass and concentration / halo mass
         relations from simulations are used as priors. Otherwise flat priors.
         Default is True.
-    infer_errors : bool
+    **infer_errors : bool
         Whether additional error component is added in quadrature to observed
         error. Additional component is then a free parameter in the fit.
         Default is True.
     **fR_parameter : str, {'free', 'fixed'}
         If fR is True, then whether fR0 is a free parameter, or an imposed
-        value.
+        value. Default is 'free'.
     **log10fR0 : float
         If fR is True and fR_parameter is 'fixed', then the (log10 of the)
         value of the imposed fR0. For example, for F6, log10fR0 would be -6.0.
@@ -72,6 +75,10 @@ class GalaxyFit:
 
     Attributes
     ----------
+    All parameters described above are stored as attributes, in addition to:
+
+    name : str
+        Name of galaxy.
     ndim : int
         Number of dimensions of parameter space.
     prior_bounds_lower : numpy.ndarray, shape (ndim,)
@@ -89,70 +96,29 @@ class GalaxyFit:
     Methods
     -------
     __init__ :
-        Initialises an instance of GalaxyFit.
+        Initialise an instance of GalaxyFit.
+    read_kwargs :
+        Internal method to parse kwargs fed to __init__
     initialise_pos :
-        Randomly finds initial positions for the MCMC walkers.
+        Randomly find initial positions for the MCMC walkers.
     iterate :
-        Runs the MCMC.
+        Run the MCMC.
 
     """
-    def __init__(self, galaxy, nwalkers=30, ntemps=4, fR=False,
-                 halo_type='NFW', upsilon='single', baryon_bound=True,
-                 scaling_priors=True, infer_errors=True, **kwargs):
+    def __init__(self, galaxy, nwalkers=30, ntemps=4, **kwargs):
         """
-        Initialises an instance of GalaxyFit class, see GalaxyFit docstring for
+        Initialise an instance of GalaxyFit class, see GalaxyFit docstring for
         more info.
         """
 
-        # storing parameters
+        # store parameters
         self.galaxy = galaxy
         self.name = galaxy.name
         self.nwalkers = nwalkers
         self.ntemps = ntemps
-        self.fR = fR
-        self.halo_type = halo_type
-        if halo_type not in ['NFW', 'DC14']:
-            raise ValueError("Unrecognised halo type!")
-        self.upsilon = upsilon
-        if upsilon not in ['single', 'fixed', 'double']:
-            raise ValueError("Unrecognised mass-to-light ratio prescription!")
-        self.baryon_bound = baryon_bound
-        self.scaling_priors = scaling_priors
-        self.infer_errors = infer_errors
 
-        # check kwargs all understood
-        acceptable_kwargs = ['fR_parameter', 'log10fR0', 'stellar_screening',
-                             'env_screening', 'MG_grid_dim']
-        for k in kwargs.keys():
-            if k not in acceptable_kwargs:
-                raise KeyError("Unrecognised key: "+k+" in GalaxyFit")
-
-        # read in required kwargs
-        if self.fR:
-            if 'fR_parameter' in kwargs.keys():
-                self.fR_parameter == kwargs['fR_parameter']
-                if self.fR_parameter not in ['fixed', 'free']:
-                    raise ValueError("fR_parameter_type should be"
-                                     "'fixed' or 'free'")
-            else:
-                raise KeyError("Need key 'fR_parameter' for f(R) fit")
-
-            if self.fR_parameter == 'fixed':
-                if 'log10fR0' in kwargs.keys():
-                    self.log10fR0 = kwargs['log10fR0']
-                    if self.log10fR0 > -5.6 or self.log10fR0 < -9:
-                        raise ValueError("Invalid value for fR0.")
-                else:
-                    raise KeyError("Need 'log10fR0' if fixing fR0")
-
-            self.stellar_screening = kwargs.get('stellar_screening', False)
-            self.env_screening = kwargs.get('env_screening', False)
-            self.MG_grid_dim = kwargs.get('MG_grid_dim', 1)
-            if self.MG_grid_dim not in [1, 2]:
-                raise ValueError("Require 1 or 2 for MG_grid_dim")
-        else:
-            if len(kwargs.keys()) > 0:
-                raise TypeError("Too many keyword arguments in GalaxyFit!")
+        # read and store kwargs
+        self.read_kwargs(kwargs)
 
         # assign number of free parameters ndim, as well as lower and upper
         # bounds on priors lb and ub. Start with 2 NFW, then add 0, 1, or 2
@@ -197,7 +163,124 @@ class GalaxyFit:
 
         return
 
+    def read_kwargs(self, kwargs):
+        """Internal method to parse kwargs fed to __init__"""
+
+        # check kwargs all understood
+        acceptable_kwargs = ['model', 'fR', 'halo_type', 'upsilon',
+                             'baryon_bound', 'scaling_priors',
+                             'infer_errors', 'fR_parameter',
+                             'log10fR0', 'stellar_screening',
+                             'env_screening', 'MG_grid_dim']
+        for k in kwargs.keys():
+            if k not in acceptable_kwargs:
+                raise KeyError("Unrecognised key: "+k+" in GalaxyFit")
+
+        # apply settings for preset models
+        if 'model' in kwargs.keys():
+            if len(kwargs.keys()) > 1:
+                raise KeyError("Additional kwargs with preset model")
+            self.model = kwargs['model']
+
+            model_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+            for i in range(20):
+                model_list.append('H'+str(i))
+                model_list.append('I'+str(i))
+            if self.model not in model_list:
+                raise ValueError("Unrecognised model!")
+
+            if self.model in ['A', 'G']:
+                self.fR = False
+            else:
+                self.fR = True
+                self.MG_grid_dim = 1
+
+            if self.model not in ['E', 'F']:
+                self.upsilon = 'single'
+            elif self.model == 'E':
+                self.upsilon = 'fixed'
+            else:
+                self.upsilon = 'double'
+
+            if self.model[0] in ['G', 'I']:
+                self.halo_type = 'DC14'
+            else:
+                self.halo_type = 'NFW'
+
+            if self.fR is True:
+                if self.model == 'C':
+                    self.env_screening = True
+                else:
+                    self.env_screening = False
+
+                if self.model == 'D':
+                    self.stellar_screening = True
+                else:
+                    self.stellar_screening = False
+
+                if self.model[0] not in ['H', 'I']:
+                    self.fR_parameter = 'free'
+                else:
+                    self.fR_parameter = 'fixed'
+                    ind = int(self.model[1:])
+                    vals = np.linspace(np.log10(1.563e-8), np.log10(2e-6), 20)
+                    self.log10fR0 = vals[ind]
+
+            self.baryon_bound = True
+            self.scaling_priors = True
+            self.infer_errors = True
+
+        # read in custom settings if model not preset
+        else:
+
+            self.fR = kwargs.get('fR', False)
+            self.halo_type = kwargs.get('halo_type', 'NFW')
+            if self.halo_type not in ['NFW', 'DC14']:
+                raise ValueError("Unrecognised halo type!")
+            self.upsilon = kwargs.get('upsilon', 'single')
+            if self.upsilon not in ['single', 'fixed', 'double']:
+                raise ValueError("Unrecognised ML ratio prescription!")
+            self.baryon_bound = kwargs.get('baryon_bound', True)
+            self.scaling_priors = kwargs.get('scaling_priors', True)
+            self.infer_errors = kwargs.get('infer_errors', True)
+
+            # f(R)-specific settings
+            if self.fR:
+
+                self.fR_parameter = kwargs.get('fR_parameter', 'free')
+                if self.fR_parameter not in ['fixed', 'free']:
+                    raise ValueError("fR_parameter_type should be"
+                                     "'fixed' or 'free'")
+
+                if self.fR_parameter == 'fixed':
+                    if 'log10fR0' in kwargs.keys():
+                        self.log10fR0 = kwargs['log10fR0']
+                        if self.log10fR0 > -5.6 or self.log10fR0 < -9:
+                            raise ValueError("Invalid value for fR0.")
+                    else:
+                        raise KeyError("Need 'log10fR0' if fixing fR0")
+                else:
+                    if 'log10fR0' in kwargs.keys():
+                        raise KeyError("No 'log10fR0' if varying fR0")
+
+                self.stellar_screening = kwargs.get('stellar_screening', False)
+                self.env_screening = kwargs.get('env_screening', False)
+                self.MG_grid_dim = kwargs.get('MG_grid_dim', 1)
+                if self.MG_grid_dim not in [1, 2]:
+                    raise ValueError("Require 1 or 2 for MG_grid_dim")
+
+            # check no f(R) settings given for LCDM run
+            else:
+                bad_kwargs = ['fR_parameter', 'log10fR0', 'stellar_screening',
+                              'env_screening', 'MG_grid_dim']
+                for k in bad_kwargs:
+                    if k in kwargs.keys():
+                        raise KeyError(k+" specified in LCDM run.")
+
+        return
+
     def initialise_pos(self):
+        """Randomly find initial positions for the MCMC walkers."""
 
         lb = self.prior_bounds_lower
         ub = self.prior_bounds_upper
@@ -208,14 +291,14 @@ class GalaxyFit:
 
         # making sure all initial postions are not in region forbidden by
         # cosmic baryon bound; shifting them if they are
-        if self.CosmicBaryonBound:
+        if self.baryon_bound:
             for i in range(self.ntemps):
                 for j in range(self.nwalkers):
 
-                    BoundViolated = True
-                    while BoundViolated:
+                    bound_violated = True
+                    while bound_violated:
                         theta = p0[i, j]
-                        if self.ML_ratio == 'fixed':
+                        if self.upsilon == 'fixed':
                             ML = 0.5
                         else:
                             ML = 10**theta[self.theta_dict['ML_disc']]
@@ -226,35 +309,52 @@ class GalaxyFit:
                         if (M_star+M_gas)/M_halo > 0.2:
                             p0[i, j] = lb + d*np.random.rand(self.ndim)
                         else:
-                            BoundViolated = False
+                            bound_violated = False
 
         return p0
 
-    def iterate(self, niter, threads=1, initial_run=True, p0=None):
+    def iterate(self, niter, threads=1, initial_run=True, print_progress=True):
         """
-        Perform niter iterations
+        Run MCMC.
+
+        Parameters
+        ----------
+        niter : int
+            Number of MCMC iterations per walker.
+        threads : int
+            Number of CPU threads to run MCMC on. Default is 1.
+        initial_run : bool
+            Whether the present run is the first of the MCMC, or if it is a
+            continuation of a previous run. In the latter case, initial
+            positions of the walkers are set to last point on the chain.
+        print_progress : bool
+            Whether to output percentage completion every 100 iterations
         """
 
-        loglargs = [self.theta_dict, self.galaxy,
-                    self.fR_parameter, self.log10fR0, self.Rscr_fixed,
-                    self.halo_type, self.StellarScreening, self.EnvScreening,
-                    self.ML_ratio, self.infer_errors, self.MGGridDim,
-                    self.Verbose]
-        logpargs = [self.theta_dict,
-                    self.prior_bounds_lower, self.prior_bounds_upper,
-                    self.galaxy, self.halo_type, self.ML_ratio,
-                    self.CosmicBaryonBound, self.scaling_priors]
+        # kwargs for prior and likelihood functions
+        kwargs = {'theta_dict': self.theta_dict, 'halo_type': self.halo_type,
+                  'upsilon': self.upsilon, 'infer_errors': self.infer_errors,
+                  'galaxy': self.galaxy, 'scaling_priors': self.scaling_priors,
+                  'baryon_bound': self.baryon_bound, 'fR': self.fR,
+                  'prior_bounds_lower': self.prior_bounds_lower,
+                  'prior_bounds_upper': self.prior_bounds_upper}
+        if self.fR:
+            kwargs['fR_parameter'] = self.fR_parameter
+            if self.fR_parameter == 'fixed':
+                kwargs['log10fR0'] = self.log10fR0,
+            kwargs['stellar_screening'] = self.stellar_screening
+            kwargs['env_screening'] = self.env_screening
+            kwargs['MG_grid_dim'] = self.MG_grid_dim
+
+        # initialise sampler
         sampler = emcee.PTSampler(self.ntemps, self.nwalkers, self.ndim,
                                   lnlike, lnprior, threads=threads,
-                                  loglargs=loglargs, logpargs=logpargs)
+                                  loglkwargs=kwargs, logpkwargs=kwargs)
 
         # either initialise walker positions or continue existing chain
         if initial_run:
             self.niter = niter
-            if p0 is None:
-                p0 = self.initialise_pos()
-            else:
-                assert p0.shape == (self.ntemps, self.nwalkers, self.ndim)
+            p0 = self.initialise_pos()
         else:
             self.niter += niter
             p0 = self.chain[:, :, -1, :]
@@ -262,7 +362,7 @@ class GalaxyFit:
         # run MCMC
         t0 = time.time()
         for i, result in enumerate(sampler.sample(p0, iterations=niter)):
-            if (i+1) % 10 == 0:
+            if (i+1) % 100 == 0:
                 sys.stdout.write("{0:5.1%}\n".format(float(i+1) / niter))
                 sys.stdout.flush()
         t1 = time.time()
@@ -280,15 +380,11 @@ class GalaxyFit:
         return
 
 
+def lnlike(theta, **kwargs):
+    """
+    Gaussian log-likelihood function
 
-# MCMC log likelihood
-def lnlike(theta, theta_dict, galaxy, fR_parameter, log10fR0, Rscr_fixed,
-           halo_type, StellarScreening, EnvScreening, ML_ratio, infer_errors,
-           MGGridDim, Verbose):
-
-    if Verbose:
-        print("Parameters: ", theta)
-
+    """
     model = v_model(theta=theta, theta_dict=theta_dict, galaxy=galaxy,
                     fR_parameter=fR_parameter, log10fR0=log10fR0,
                     Rscr_fixed=Rscr_fixed,
@@ -296,16 +392,10 @@ def lnlike(theta, theta_dict, galaxy, fR_parameter, log10fR0, Rscr_fixed,
                     StellarScreening=StellarScreening,
                     EnvScreening=EnvScreening,
                     ML_ratio=ML_ratio, MGGridDim=MGGridDim)
-    if infer_errors:
+    if kwargs['infer_errors']:
         sigma = np.sqrt(galaxy.v_err**2 + theta[theta_dict['sigma_gal']]**2)
         lnlike = -np.sum(0.5*((galaxy.v - model)/sigma)**2 + np.log(sigma))
     else:
         lnlike = -0.5*np.sum(((galaxy.v - model)/galaxy.v_err)**2)
 
     return lnlike
-
-
-if __name__ == '__main__':
-
-    gal = spam.data.sample_standard[0]
-    fit = GalaxyFit(gal)

dcc9df975298312c1b93aa803a9ef77ef883e716
Aneesh Naik <aneesh92@hotmail.co.uk>
2019-03-28 13:43:48 +0000

    Finish FitSummary class

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -7,6 +7,8 @@ Description: init file of 'analysis' submodule.
 """
 import os as _os
 import pickle as _pickle
+import spam as _spam
+import util as _util
 import numpy as _np
 
 
@@ -31,26 +33,77 @@ def open_fit(model, name, fitdir=_os.environ['SPAMFITDIR']):
     return fit
 
 
-def lnL(fit):
-
-    data = fit.galaxy.v
-    model = fit.maxprob_v_circ
-    err = fit.maxprob_v_err
-
+def _lnL(data, model, err):
     lnL = -0.5*_np.sum(((data-model)**2/err**2)+_np.log(2*_np.pi*err**2))
     return lnL
 
 
-def BIC(fit):
+def _BIC(n, k, lnL):
     """
     BIC = ln(n)k - 2 lnL
     """
-    n = fit.galaxy.R.size
-    k = fit.ndim
-
-    BIC = _np.log(n)*k - 2*lnL(fit)
-
+    BIC = _np.log(n)*k - 2*lnL
     return BIC
 
 
-__all__ = ['open_fit', 'open_summary']
+class FitSummary():
+    def __init__(self, model, sample='standard',
+                 fitdir=_os.environ['SPAMFITDIR']):
+
+        self.model = model
+        self.sample = sample
+        if sample == 'standard':
+            names = _spam.data.names_standard
+        else:
+            names = _spam.data.names_full
+
+        # loop over galaxies
+        print("Model "+model+": looping over galaxies...")
+        self.galaxies = {}
+        for name in names:
+
+            # progress bar
+            _util.print_progress(names.index(name), len(names))
+
+            # load galaxy data and fit
+            galaxy = _spam.data.SPARCGalaxy(name)
+            fit = _spam.analysis.open_fit(model, name, fitdir)
+
+            # find maximum posterior parameters
+            lnprob = fit.lnprob[0, :, -5000:]
+            ind = _np.unravel_index(_np.argmax(lnprob), lnprob.shape)
+            chain = fit.chain[0, :, -5000:, :]
+            theta = chain[ind]
+            galaxy.maxprob_theta = theta
+
+            # calculate velocity for best fit model
+            kw = fit.__dict__
+            v = _spam.fit.rotcurve.v_model(theta, component_split=True, **kw)
+            galaxy.maxprob_v_circ = v[0]
+            galaxy.maxprob_v_gas = v[1]
+            galaxy.maxprob_v_disc = v[2]
+            galaxy.maxprob_v_bulge = v[3]
+            galaxy.maxprob_v_DM = v[4]
+            galaxy.maxprob_v_5 = v[5]
+
+            # calculate errors if fitting
+            if fit.infer_errors:
+                sigma_gal = theta[fit.theta_dict['sigma_gal']]
+                err = _np.sqrt(galaxy.v_err**2 + sigma_gal**2)
+                galaxy.maxprob_v_err = err
+                galaxy.lnL = _lnL(data=galaxy.v, model=v[0], err=err)
+            else:
+                galaxy.lnL = _lnL(data=galaxy.v, model=v[0], err=galaxy.v_err)
+
+            # calculate BIC
+            n = fit.galaxy.R.size
+            k = fit.ndim
+            galaxy.BIC = _BIC(n, k, galaxy.lnL)
+
+            # save galaxy
+            self.galaxies[name] = galaxy
+
+        return
+
+
+__all__ = ['open_fit', 'open_summary', 'FitSummary']
--- analysis/summary.py
+++ analysis/summary.py
@@ -7,37 +7,10 @@ Description:
 """
 import spam
 import os
+import numpy as np
+from util import print_progress
 
 
-class FitSummary():
-    def __init__(self, model, sample='standard',
-                 fitdir=os.environ['SPAMFITDIR']):
 
-        self.model = model
-        self.sample = sample
-        if sample == 'standard':
-            names = spam.data.names_standard
-        else:
-            names = spam.data.names_full
 
-        self.galaxies = {}
-        for name in names:
 
-            galaxy = spam.data.SPARCGalaxy(name)
-            fit = spam.analysis.open_fit(model, name, fitdir)
-
-            galaxy.maxprob_theta = fit.maxprob_theta
-            galaxy.maxprob_v_circ = fit.maxprob_v_circ
-            galaxy.maxprob_v_gas = fit.maxprob_v_gas
-            galaxy.maxprob_v_disc = fit.maxprob_v_disc
-            galaxy.maxprob_v_bulge = fit.maxprob_v_bulge
-            galaxy.maxprob_v_DM = fit.maxprob_v_DM
-            galaxy.maxprob_v_5 = fit.maxprob_v_5
-            if fit.infer_errors:
-                galaxy.maxprob_v_err = fit.maxprob_v_err
-
-            galaxy.lnL = lnL(fit)
-            galaxy.BIC = BIC(fit)
-
-            self.galaxies[name] = galaxy
-        return
